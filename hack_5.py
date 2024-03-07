5187c3146caa5e86a0dafd26b4cd673484a813c0
Royakuro <wegotthispls@gmail.com>
2024-03-06 20:17:02 -0400

    Entrega de tarea

--- hack_5.py
+++ hack_5.py
@@ -4,5 +4,5 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    #...
-    return result  
+    result = result.replace("o", "0").replace("i", "1").replace("a", "@")
+    return result
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,10 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
-    return result  
+    i = 0
+    while i < 7:
+        result.insert(i+1, '@')
+        i += i+2
+    return result
+
+fn_hack_9()
